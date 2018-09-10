ecd6b72d989f94c97cea2cdc678b592bf8a43ffc
Sergey Kondratiev <mailto:sergeykondr@gmail.com>
2018-09-11 02:42:14 +0300

    1.4. JUnit

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -3,11 +3,24 @@ package ru.job4j.calculate;
  * Class Класс Калькулятор
  * @author sergey
  * @since 11.09.2018
- * @version 1
+ * @version 0.1
  */
 public class Calculate {
-		
+		/**
+		* Main.
+		* @param args - args.
+		*/
 		public static void main(String[] args) {
 			System.out.println("Hello my world!");
+		}
+		
+		
+		/**
+		* Method echo.
+		* @param name Your name.
+		* @return Echo plus your name.
+		*/
+		public String echo(String name) {
+			return "Echo, echo, echo : " + name;
 		}
 }
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * //TODO add comments.
+ *
+ * @author Sergey K (sergeyk@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j;
