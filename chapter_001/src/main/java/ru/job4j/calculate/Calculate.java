531e4efc898d8df96fae6ce3e2bd749925203860
Sergey Kondratiev <mailto:sergeykondr@gmail.com>
2018-09-11 01:49:41 +0300

    1.3. JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,10 @@
 package ru.job4j.calculate;
-
+/**
+ * Class Класс Калькулятор
+ * @author sergey
+ * @since 11.09.2018
+ * @version 1
+ */
 public class Calculate {
 		
 		public static void main(String[] args) {
